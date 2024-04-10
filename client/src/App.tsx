997afd584e274389cb55031418b48d40fb21ab87
JamesErrington <james.errington@protonmail.com>
2024-04-10 19:22:11 +0100

    Add ErrorBoundary error handling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,10 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+	QueryClient,
+	QueryClientProvider,
+	QueryErrorResetBoundary,
+} from "@tanstack/react-query";
 import type { FC } from "react";
+import { ErrorBoundary, type FallbackProps } from "react-error-boundary";
 import { FirmTable } from "./components/FirmTable";
 
 const queryClient = new QueryClient();
@@ -7,7 +12,24 @@ const queryClient = new QueryClient();
 export const App: FC = () => {
 	return (
 		<QueryClientProvider client={queryClient}>
-			<FirmTable />
+			<QueryErrorResetBoundary>
+				{({ reset }) => (
+					<ErrorBoundary onReset={reset} fallbackRender={fallbackRender}>
+						<FirmTable />
+					</ErrorBoundary>
+				)}
+			</QueryErrorResetBoundary>
 		</QueryClientProvider>
+	);
+};
+
+const fallbackRender = ({ resetErrorBoundary }: FallbackProps) => {
+	return (
+		<div className="error">
+			<h3>An error occured. Please try again</h3>
+			<button type="button" onClick={resetErrorBoundary}>
+				Reload
+			</button>
+		</div>
 	);
 };
