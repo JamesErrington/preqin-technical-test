2b5a2758667fb7a305cec83816753331c2f710c6
JamesErrington <james.errington@protonmail.com>
2024-04-10 19:15:16 +0100

    Add TanStack Table to display the firm data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { FC } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { FirmTable } from "./components/FirmTable"
+import { FirmTable } from "./components/FirmTable";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export const App: FC = () => {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<FirmTable />
 		</QueryClientProvider>
-	)
+	);
 };
--- client/src/components/FirmTable.tsx
+++ client/src/components/FirmTable.tsx
@@ -1,21 +1,88 @@
 import { useQuery } from "@tanstack/react-query";
+import {
+	createColumnHelper,
+	flexRender,
+	getCoreRowModel,
+	useReactTable,
+} from "@tanstack/react-table";
 import type { FC } from "react";
+import "./FirmTable.css";
 
 function useFirms() {
-    return useQuery<Firm[]>({
-        queryKey: ['firms'],
-        queryFn: () => fetch("http://localhost:8000/api/investors").then(res => res.json()),
-        initialData: [],
-        throwOnError: true,
-        retry: false,
-    })
+	return useQuery<Firm[]>({
+		queryKey: ["firms"],
+		queryFn: () =>
+			fetch("http://localhost:8000/api/investors").then((res) => res.json()),
+		initialData: [],
+		throwOnError: true,
+		retry: false,
+	});
 }
 
+const columnHelper = createColumnHelper<Firm>();
+
+const columns = [
+	columnHelper.accessor("firm_id", {
+		header: () => <span>ID</span>,
+		cell: (info) => info.renderValue(),
+	}),
+	columnHelper.accessor("firm_name", {
+		header: () => <span>Name</span>,
+		cell: (info) => info.renderValue(),
+	}),
+	columnHelper.accessor("firm_type", {
+		header: () => <span>Type</span>,
+		cell: (info) => info.renderValue(),
+	}),
+	columnHelper.accessor("date_added", {
+		header: () => <span>Date Added</span>,
+		cell: (info) => new Date(info.getValue()).toLocaleDateString("en-UK"),
+	}),
+	columnHelper.accessor("address", {
+		header: () => <span>Address</span>,
+		cell: (info) => info.renderValue(),
+	}),
+];
+
 export const FirmTable: FC = () => {
-    const { data: firms } = useFirms()
-    return (
-        <div className="firm-table">
-            {firms.map(firm => <>{firm.firm_name}</>)}
-        </div>
-    );
-}
+	const { data } = useFirms();
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+	});
+
+	return (
+		<div className="firm-table">
+			<table>
+				<thead>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<tr key={headerGroup.id}>
+							{headerGroup.headers.map((header) => (
+								<th key={header.id}>
+									{header.isPlaceholder
+										? null
+										: flexRender(
+												header.column.columnDef.header,
+												header.getContext(),
+											)}
+								</th>
+							))}
+						</tr>
+					))}
+				</thead>
+				<tbody>
+					{table.getRowModel().rows.map((row) => (
+						<tr key={row.id}>
+							{row.getVisibleCells().map((cell) => (
+								<td key={cell.id}>
+									{flexRender(cell.column.columnDef.cell, cell.getContext())}
+								</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
